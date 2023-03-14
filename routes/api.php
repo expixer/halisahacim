539526746ed7712bf3c0afb57092e8f33fb58658
Sezgin Sevinc <sezginex@gmail.com>
2023-03-14 19:50:09 +0300

    cors added

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,14 @@ use \App\Http\Controllers\Api\V1\Auth;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware(['cors'])->group(function () {
+    Route::post('auth/register', Auth\RegisterController::class);
+    Route::post('auth/login', Auth\LoginController::class);
 
-Route::post('auth/register', Auth\RegisterController::class);
-Route::post('auth/login', Auth\LoginController::class);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('profile', [Auth\ProfileController::class, 'show']);
-    Route::put('profile', [Auth\ProfileController::class, 'update']);
-    Route::put('password', Auth\PasswordUpdateController::class);
-    Route::post('auth/logout', Auth\LogoutController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('profile', [Auth\ProfileController::class, 'show']);
+        Route::put('profile', [Auth\ProfileController::class, 'update']);
+        Route::put('password', Auth\PasswordUpdateController::class);
+        Route::post('auth/logout', Auth\LogoutController::class);
+    });
 });
