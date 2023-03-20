00cceb002cc23a5bd2186951622176db8e46d53f
Sezgin Sevinc <sezginex@gmail.com>
2023-03-20 17:04:44 +0300

    stadiums apiresources

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware(['cors'])->group(function () {
         Route::put('profile', [Auth\ProfileController::class, 'update']);
         Route::put('password', Auth\PasswordUpdateController::class);
         Route::post('auth/logout', Auth\LogoutController::class);
+        Route::apiResource('stadiums', V1\StadiumController::class);
     });
-    Route::apiResource('stadiums', V1\StadiumController::class);
 
 });
