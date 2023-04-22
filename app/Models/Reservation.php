49f976be72bebc956edbe885dcb7aa1dec274fe4
inheritance10 <ali552001a@gmail.com>
2023-04-22 22:07:39 +0300

    update

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -66,7 +66,7 @@ class Reservation extends Model
     public function scopeOldMatches($query)
     {
         $now = Carbon::now();
-        $query->with('stadium')->where('match_date', '<=', $now->format('Y-m-d'));
+        $query->with('stadium')->where('match_date', '<', $now->format('Y-m-d'));
 
         if ($now->format('Y-m-d') === $this->match_date) {
             $query->where('match_time', '<=', $now->format('H:i:s'));
