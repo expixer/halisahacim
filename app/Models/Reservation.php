c2383febda8b5873cfc73aa24b21f84c99d618f3
Sezgin Sevinc <sezginex@gmail.com>
2023-04-23 16:22:49 +0300

    active matches scope edited

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -36,31 +36,30 @@ class Reservation extends Model
         'price' => 'double',
     ];
 
-    public function getMatchDateTimeAttribute()
+    public function getMatchDateTimeAttribute(): string
     {
         return Carbon::parse($this->match_date)->format('Y-m-d') . ' ' . Carbon::parse($this->match_time)->format('H:i:s');
     }
 
-    public function user()
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function stadium()
+    public function stadium(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Stadium::class);
     }
 
-    public function scopeActiveMatches($query)
+    public function scopeActiveMatches($query): void
     {
         $now = Carbon::now();
+        $query->with('stadium')->where('match_date', '<=', $now->format('Y-m-d'));
 
-        $query->with('stadium')
-            ->where('match_date', '>=', $now->format('Y-m-d'))
-            ->where('match_time', '>', $now->format('H:i:s'))
-            ->orderBy('match_date', 'desc');
-
-        return $query;
+        if ($now->format('Y-m-d') === $this->match_date) {
+            $query->where('match_time', '<=', $now->format('H:i:s'));
+        }
+        $query->orderBy('match_date', 'desc');
     }
 
     public function scopeOldMatches($query)
