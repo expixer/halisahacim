b3d5565543ad6116e5a829c5ef206c3d0dc0a1b5
Sezgin Sevinc <sezginex@gmail.com>
2024-07-26 14:24:47 +0000

    inertia vuejs to livewire

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,21 @@
 <?php
-
 use App\Http\Controllers\Auth\VerifyMobileController;
 use App\Http\Controllers\TempController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('gitpull', [TempController::class, 'gitpull']);
-
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-    'verify.mobile',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
+        return view('dashboard');
     })->name('dashboard');
 });
 
