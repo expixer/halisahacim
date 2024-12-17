ae28811a0b1029e8a2475d146048e4a1903885e1
Sezgin Sevinc <sezginex@gmail.com>
2024-12-17 16:33:31 +0300

    init firebase notification

--- app/Broadcasting/FcmChannel.php
+++ app/Broadcasting/FcmChannel.php
@@ -7,33 +7,30 @@ use GuzzleHttp\Client;
 use Illuminate\Notifications\Notification;
 
 
-class FcmChannel
-{
-    /**
-     * Create a new channel instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+class FcmChannel {
+  /**
+   * Create a new channel instance.
+   */
+  public function __construct() {
+    //
+  }
 
-    /**
-     * Authenticate the user's access to the channel.
-     */
-    public function join(User $user): array|bool
-    {
-        return true;
-    }
-  public function send($notifiable, Notification $notification)
-  {
+  /**
+   * Authenticate the user's access to the channel.
+   */
+  public function join(User $user): array|bool {
+    return true;
+  }
+
+  public function send($notifiable, Notification $notification) {
     $message = $notification->toFcm($notifiable);
     $client = new Client();
     $response = $client->post('https://fcm.googleapis.com/fcm/send', [
       'headers' => [
         'Authorization' => 'key=' . env('FCM_SERVER_KEY'),
-        'Content-Type' => 'application/json',
+        'Content-Type'  => 'application/json',
       ],
-      'json' => $message,
+      'json'    => $message,
     ]);
 
     return $response;
